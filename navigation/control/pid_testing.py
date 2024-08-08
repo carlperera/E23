1d6d621b388b6d89cfd2cadb60f0231005e27a8d
admin@e23pi <elim0064@student.monash.edu>
2024-08-08 01:23:31 +0100

    stuff

---
+++ navigation/control/pid_testing.py
@@ -0,0 +1,20 @@
+import gpiozero
+import time
+
+pwm = gpiozero.PWMOutputDevice(pin=19,active_high=True,initial_value=0,frequency=100)
+dir1 = gpiozero.OutputDevice(pin=27)
+dir2 = gpiozero.OutputDevice(pin=17)
+encoder = gpiozero.RotaryEncoder(a=21, b=20,max_steps=100000) # encoder pins a = yellow, b = white
+# This class has a lot more functionality,so worth reading up on it
+# Step through duty cycle values, slowly increasing the speed and changing the direction of motion
+encoder.steps = 0
+for j in range(10):
+    pwm.value = j/10
+    dir1.value = not dir1.value
+    dir2.value = not dir1.value
+    print('Duty cycle:',pwm.value,'Direction:',dir1.value)
+    time.sleep(5.0)
+    print('Counter:',encoder.steps,'Speed:',(encoder.steps)/5.0,'steps per second\n')
+    encoder.steps = 0
+
+pwm.value =0 
--- navigation/control/servo_test.py
+++ navigation/control/servo_test.py
@@ -1,94 +1,149 @@
-# import RPi.GPIO as GPIO          
+# from gpiozero import AngularServo
 # from time import sleep
 
-# in1 = 17
-# in2 = 27
-# en = 25
-# temp1=1
+# servo = AngularServo(10, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(in1,GPIO.OUT)
-# GPIO.setup(in2,GPIO.OUT)
-# GPIO.setup(en,GPIO.OUT)
-# GPIO.output(in1,GPIO.LOW)
-# GPIO.output(in2,GPIO.LOW)
-# p=GPIO.PWM(en,1000)
-# p.start(25)
-# print("\n")
-# print("The default speed & direction of motor is LOW & Forward.....")
-# print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-# print("\n")    
+# while (True):
+#     servo.angle = 90
+#     sleep(2)
+#     servo.angle = 0
+#     sleep(2)
+#     servo.angle = -90
+#     sleep(2)
 
-# while(1):
+import gpiozero
+from gpiozero import Motor
+import time
+import RPi.GPIO as GPIO
 
-#     x=input()
-    
-#     if x=='r':
-#         print("run")
-#         if(temp1==1):
-#          GPIO.output(in1,GPIO.HIGH)
-#          GPIO.output(in2,GPIO.LOW)
-#          print("forward")
-#          x='z'
-#         else:
-#          GPIO.output(in1,GPIO.LOW)
-#          GPIO.output(in2,GPIO.HIGH)
-#          print("backward")
-#          x='z'
+# Set GPIO modes
+GPIO.setmode(GPIO.BCM)
+motor_A_in1 = 2 #non pwm
+motor_A_in2 = 3 # non pwm
+
+motor_B_in1 = 17 # non pwm
+motor_B_in2 = 27 # non pwm
+
+# motor_B_in2 = 24
+# motor_B_in1 = 25
+# motor_B_en = 19
+
+motor_enable = 18
+motor_enable2 = 19
+
+GPIO.setup(motor_A_in1, GPIO.OUT)
+GPIO.setup(motor_A_in2, GPIO.OUT)
+
+GPIO.setup(motor_B_in1, GPIO.OUT)
+GPIO.setup(motor_B_in2, GPIO.OUT)
+GPIO.setup(motor_enable, GPIO.OUT)
+GPIO.setup(motor_enable2, GPIO.OUT)
 
 
-#     elif x=='s':
-#         print("stop")
-#         GPIO.output(in1,GPIO.LOW)
-#         GPIO.output(in2,GPIO.LOW)
-#         x='z'
 
-#     elif x=='f':
-#         print("forward")
-#         GPIO.output(in1,GPIO.HIGH)
-#         GPIO.output(in2,GPIO.LOW)
-#         temp1=1
-#         x='z'
+enable_pwm = GPIO.PWM(motor_enable, 1000)
+enable_pwm2 = GPIO.PWM(motor_enable2, 1000)
 
-#     elif x=='b':
-#         print("backward")
-#         GPIO.output(in1,GPIO.LOW)
-#         GPIO.output(in2,GPIO.HIGH)
-#         temp1=0
-#         x='z'
+# enable_pwm.start(100) # Turning on both enables
+# enable_pwm2.start(100)
 
-#     elif x=='l':
-#         print("low")
-#         p.ChangeDutyCycle(25)
-#         x='z'
+#input 1 and input 2 pins used for motor A
+#input 1 LOW and input 2 HIGH = forward
+#input 1 HIGH and input 2 LOW = backwards
+#input 3 and input 4 pins used for motor B
+#input 3 LOW and input 4 HIGH = forward
+#input 3 HIGH and input 4 LOW = backwards
 
-#     elif x=='m':
-#         print("medium")
-#         p.ChangeDutyCycle(50)
-#         x='z'
+def forwards(speed):
+    GPIO.output(motor_A_in1, GPIO.HIGH)
+    GPIO.output(motor_B_in1, GPIO.HIGH)
+    GPIO.output(motor_A_in2, GPIO.LOW)
+    GPIO.output(motor_B_in2, GPIO.LOW)
+    enable_pwm.start(speed)
+    enable_pwm2.start(speed)    
 
-#     elif x=='h':
-#         print("high")
-#         p.ChangeDutyCycle(75)
-#         x='z'
-     
-    
-#     elif x=='e':
-#         GPIO.cleanup()
-#         break
-    
-#     else:
-#         print("<<<  wrong data  >>>")
-#         print("please enter the defined data to continue.....")import gpiozero
-from gpiozero import AngularServo
-from time import sleep
+def backwards(speed):
+    GPIO.output(motor_A_in2, GPIO.HIGH)
+    GPIO.output(motor_B_in2, GPIO.HIGH)
+    GPIO.output(motor_A_in1, GPIO.LOW)
+    GPIO.output(motor_B_in1, GPIO.LOW)
+    enable_pwm.start(speed)
+    enable_pwm2.start(speed) 
 
-servo = AngularServo(10, min_pulse_width=0.0006, max_pulse_width=0.0023)
+def right(direction, speed):
+    if direction == "f":
+        GPIO.output(motor_A_in1, GPIO.HIGH)
+        GPIO.output(motor_B_in1, GPIO.HIGH)
+        GPIO.output(motor_A_in2, GPIO.LOW)
+        GPIO.output(motor_B_in2, GPIO.LOW)
+        enable_pwm.start(speed)
+        enable_pwm2.start(speed/4) 
 
-while (True):
-    servo.angle = 90
-    sleep(2)
-    servo.angle = 0
-    sleep(2)
-    servo.angle = -90
-    sleep(2)
+    else:
+        GPIO.output(motor_A_in2, GPIO.HIGH)
+        GPIO.output(motor_B_in2, GPIO.HIGH)
+        GPIO.output(motor_A_in1, GPIO.LOW)
+        GPIO.output(motor_B_in1, GPIO.LOW)
+        enable_pwm.start(speed)
+        enable_pwm2.start(speed/4) 
+
+def left(direction, speed):
+    if direction == "f":
+        GPIO.output(motor_A_in1, GPIO.HIGH)
+        GPIO.output(motor_B_in1, GPIO.HIGH)
+        GPIO.output(motor_A_in2, GPIO.LOW)
+        GPIO.output(motor_B_in2, GPIO.LOW)
+        enable_pwm.start(speed/4)
+        enable_pwm2.start(speed) 
+
+    else:
+        GPIO.output(motor_A_in2, GPIO.HIGH)
+        GPIO.output(motor_B_in2, GPIO.HIGH)
+        GPIO.output(motor_A_in1, GPIO.LOW)
+        GPIO.output(motor_B_in1, GPIO.LOW)
+        enable_pwm.start(speed/4)
+        enable_pwm2.start(speed) 
+
+
+while True:
+    try:
+        directionFlag = input("Please state first letter of direction and speed: ")
+        direction = directionFlag.split(" ")[0]
+        speed =  int(directionFlag.split(" ")[1])
+        print(direction)
+
+        if direction == "b": # if user types "back" change direction of motor
+            backwards(speed)
+                
+        if direction == "f":
+            forwards(speed)
+
+        if direction == "bl":
+            left("b", speed)
+        if direction == "br":
+            right("b", speed)
+        if direction == "fl": # front left
+            left("f", speed)
+        if direction == "fr": # Front right
+            right("f", speed)
+        if direction == "bl":
+            left("b", speed)
+        if direction == "br":
+            right("b", speed)
+
+        if direction == "s":
+            enable_pwm.stop()
+            enable_pwm2.stop()
+
+    except KeyboardInterrupt:
+        GPIO.output(motor_A_in2, GPIO.LOW)
+        GPIO.output(motor_B_in2, GPIO.LOW)
+        GPIO.output(motor_A_in1, GPIO.LOW)
+        GPIO.output(motor_B_in1, GPIO.LOW)
+        enable_pwm.stop()
+        enable_pwm2.stop()
+
+        GPIO.cleanup()
+        print("Done")
+        break
+
