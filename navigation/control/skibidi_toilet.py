368e758c783c1386be0a4bb68e0226d1c9074942
admin@e23pi <elim0064@student.monash.edu>
2024-08-16 11:22:15 +0100

    thibg

--- navigation/control/skibidi_toilet.py
+++ navigation/control/skibidi_toilet.py
@@ -5,11 +5,14 @@ prevA = 0
 prevB = 0
 count = 0
 
-ENCODER_A_PIN = 69
-ENCODER_B_PIN = 420
 CPR = 48
-GEAR_RATIO = 75
+GEAR_RATIO = 74.83
 
+PIN_MOTOR1_IN1 = 17 # LOW - good
+PIN_MOTOR1_IN2 = 27 # LOW -good 
+PIN_MOTOR1_PWM_ENABLE = 18 # LOW - good 
+PIN_MOTOR1_A_OUT = 21# LOW - good 
+PIN_MOTOR1_B_OUT = 20 # LOW - good
 
 def interrupt_service_routine(channel) -> None:
     """ISR for the motor encoder
@@ -20,22 +23,22 @@ def interrupt_service_routine(channel) -> None:
     """
     global prevA, prevB, count
 
-    a = GPIO.input(ENCODER_A_PIN)
-    b = GPIO.input(ENCODER_B_PIN)
+    a = GPIO.input(PIN_MOTOR1_A_OUT)
+    b = GPIO.input(PIN_MOTOR1_B_OUT)
 
     # do teh logic:
     # 00 --> 01 --> 11 --> 10 is anti clock wise
     # 11 --> 01 --> 00 --> 10 is clock wise
     # increment or decrement count variable
-    if (prevA == 0 and prevB == 0 and a == 0 and b == 1) or \
-       (prevA == 0 and prevB == 1 and a == 1 and b == 1) or \
-       (prevA == 1 and prevB == 1 and a == 1 and b == 0) or \
-       (prevA == 1 and prevB == 0 and a == 0 and b == 0):
+    if (prevA == GPIO.LOW and prevB == GPIO.LOW and a == GPIO.LOW and b == GPIO.HIGH) or \
+       (prevA == GPIO.LOW and prevB == GPIO.HIGH and a == GPIO.HIGH and b == GPIO.HIGH) or \
+       (prevA == GPIO.HIGH and prevB == GPIO.HIGH and a == GPIO.HIGH and b == GPIO.LOW) or \
+       (prevA == GPIO.HIGH and prevB == GPIO.LOW and a == GPIO.LOW and b == GPIO.LOW):
         count += 1  # Clockwise
-    elif (prevA == 1 and prevB == 1 and a == 0 and b == 1) or \
-        (prevA == 0 and prevB == 1 and a == 0 and b == 0) or \
-        (prevA == 0 and prevB == 0 and a == 1 and b == 0) or \
-        (prevA == 1 and prevB == 0 and a == 1 and b == 1):
+    elif (prevA == GPIO.HIGH and prevB == GPIO.HIGH and a == GPIO.LOW and b == GPIO.HIGH) or \
+        (prevA == GPIO.LOW and prevB == GPIO.HIGH and a == GPIO.LOW and b == GPIO.LOW) or \
+        (prevA == GPIO.LOW and prevB == GPIO.LOW and a == 1 and b == GPIO.LOW) or \
+        (prevA == GPIO.HIGH and prevB == GPIO.LOW and a == GPIO.HIGH and b == GPIO.HIGH):
         count -= 1  # Counterclockwise
     else:
         #do nothing
@@ -49,15 +52,42 @@ def interrupt_service_routine(channel) -> None:
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
     # set up encoder pins as inputs:
-    GPIO.setup(ENCODER_A_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(ENCODER_B_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(PIN_MOTOR1_A_OUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(PIN_MOTOR1_B_OUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    GPIO.setup(PIN_MOTOR1_IN1, GPIO.OUT)
+    GPIO.setup(PIN_MOTOR1_IN2, GPIO.OUT)
+    GPIO.setup(PIN_MOTOR1_PWM_ENABLE, GPIO.OUT)
+
+    motor1_enable_pwm = GPIO.PWM(PIN_MOTOR1_PWM_ENABLE, 1000)
+
+
+    motor1_in1 = GPIO.PWM(PIN_MOTOR1_IN1, 1000)
+    motor1_in2 = GPIO.PWM(PIN_MOTOR1_IN2, 1000)
+
+    # # Motor 2 (right)
+    # GPIO.setup(PIN_MOTOR2_IN1, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
+    # GPIO.setup(PIN_MOTOR2_IN2, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
+    # GPIO.setup(PIN_MOTOR2_PWM_ENABLE, GPIO.OUT, pull_up_down=GPIO.PUD_DOWN)
+
+    # motor2_enable_pwm = GPIO.PWM(PIN_MOTOR2_PWM_ENABLE, 1000)
+
+    # GPIO.setup(PIN_MOTOR2_A_OUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    # GPIO.setup(PIN_MOTOR2_B_OUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+    motor1_enable_pwm.start(100) 
+
+    # motor2_enable_pwm.start(100) 
+    motor1_in1.start(100)
+
 
     # set the callback function to be called:
     GPIO.add_event_detect(
-        ENCODER_A_PIN, edge=GPIO.BOTH, callback=interrupt_service_routine
+        PIN_MOTOR1_A_OUT, edge=GPIO.BOTH, callback=interrupt_service_routine
     )
     GPIO.add_event_detect(
-        ENCODER_B_PIN, edge=GPIO.BOTH, callback=interrupt_service_routine
+        PIN_MOTOR1_B_OUT, edge=GPIO.BOTH, callback=interrupt_service_routine
     )
 
     timeInterval = 0.5  # calculate speed every 0.5 seconds
@@ -78,5 +108,6 @@ if __name__ == "__main__":
 
             # honk shoo honk shoo
             time.sleep(timeInterval)
+
     except KeyboardInterrupt:
         GPIO.cleanup()
