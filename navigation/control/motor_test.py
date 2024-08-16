98c1ab4692884df0aaa7481ce6812bd49233b5b8
admin@e23pi <elim0064@student.monash.edu>
2024-08-16 08:59:58 +0100

    Encoder steps working for left,right,both motors

---
+++ navigation/control/motor_test.py
@@ -0,0 +1,152 @@
+# from gpiozero import AngularServo
+# from time import sleep
+
+# servo = AngularServo(10, min_pulse_width=0.0006, max_pulse_width=0.0023)
+
+# while (True):
+#     servo.angle = 90
+#     sleep(2)
+#     servo.angle = 0
+#     sleep(2)
+#     servo.angle = -90
+#     sleep(2)
+
+import gpiozero
+from gpiozero import Motor
+import time
+import RPi.GPIO as GPIO
+
+# Set GPIO modes
+GPIO.setmode(GPIO.BCM)
+
+
+motor_A_in1 = 17 # non pwm 7 from top left
+motor_A_in2 = 27 # non pwm 6 from top left
+
+motor_B_in1 = 23 #non pwm 3 from top left
+motor_B_in2 = 24 # non pwm 2 from top left
+
+# motor_B_in2 = 24
+# motor_B_in1 = 25
+# motor_B_en = 19
+
+motor_enable = 18 # 6 from top right
+motor_enable2 = 10 # 3 from bottom left
+
+
+GPIO.setup(motor_A_in1, GPIO.OUT)
+GPIO.setup(motor_A_in2, GPIO.OUT)
+
+GPIO.setup(motor_B_in1, GPIO.OUT)
+GPIO.setup(motor_B_in2, GPIO.OUT)
+GPIO.setup(motor_enable, GPIO.OUT)
+GPIO.setup(motor_enable2, GPIO.OUT)
+
+
+
+enable_pwm = GPIO.PWM(motor_enable, 1000)
+enable_pwm2 = GPIO.PWM(motor_enable2, 1000)
+
+# enable_pwm.start(100) # Turning on both enables
+# enable_pwm2.start(100)
+
+#input 1 and input 2 pins used for motor A
+#input 1 LOW and input 2 HIGH = forward
+#input 1 HIGH and input 2 LOW = backwards
+#input 3 and input 4 pins used for motor B
+#input 3 LOW and input 4 HIGH = forward
+#input 3 HIGH and input 4 LOW = backwards
+
+def forwards(speed):
+    GPIO.output(motor_A_in1, GPIO.HIGH)
+    GPIO.output(motor_B_in1, GPIO.HIGH)
+    GPIO.output(motor_A_in2, GPIO.LOW)
+    GPIO.output(motor_B_in2, GPIO.LOW)
+    enable_pwm.start(speed)
+    enable_pwm2.start(speed)    
+
+def backwards(speed):
+    GPIO.output(motor_A_in2, GPIO.HIGH)
+    GPIO.output(motor_B_in2, GPIO.HIGH)
+    GPIO.output(motor_A_in1, GPIO.LOW)
+    GPIO.output(motor_B_in1, GPIO.LOW)
+    enable_pwm.start(speed)
+    enable_pwm2.start(speed) 
+
+def right(direction, speed):
+    if direction == "f":
+        GPIO.output(motor_A_in1, GPIO.HIGH)
+        GPIO.output(motor_B_in1, GPIO.HIGH)
+        GPIO.output(motor_A_in2, GPIO.LOW)
+        GPIO.output(motor_B_in2, GPIO.LOW)
+        enable_pwm.start(speed)
+        enable_pwm2.start(speed/4) 
+
+    else:
+        GPIO.output(motor_A_in2, GPIO.HIGH)
+        GPIO.output(motor_B_in2, GPIO.HIGH)
+        GPIO.output(motor_A_in1, GPIO.LOW)
+        GPIO.output(motor_B_in1, GPIO.LOW)
+        enable_pwm.start(speed)
+        enable_pwm2.start(speed/4) 
+
+def left(direction, speed):
+    if direction == "f":
+        GPIO.output(motor_A_in1, GPIO.HIGH)
+        GPIO.output(motor_B_in1, GPIO.HIGH)
+        GPIO.output(motor_A_in2, GPIO.LOW)
+        GPIO.output(motor_B_in2, GPIO.LOW)
+        enable_pwm.start(speed/4)
+        enable_pwm2.start(speed) 
+
+    else:
+        GPIO.output(motor_A_in2, GPIO.HIGH)
+        GPIO.output(motor_B_in2, GPIO.HIGH)
+        GPIO.output(motor_A_in1, GPIO.LOW)
+        GPIO.output(motor_B_in1, GPIO.LOW)
+        enable_pwm.start(speed/4)
+        enable_pwm2.start(speed) 
+
+
+while True:
+    try:
+        directionFlag = input("Please state first letter of direction and speed: ")
+        direction = directionFlag.split(" ")[0]
+        speed =  int(directionFlag.split(" ")[1])
+        print(direction)
+
+        if direction == "b": # if user types "back" change direction of motor
+            backwards(speed)
+                
+        if direction == "f":
+            forwards(speed)
+
+        if direction == "bl":
+            left("b", speed)
+        if direction == "br":
+            right("b", speed)
+        if direction == "fl": # front left
+            left("f", speed)
+        if direction == "fr": # Front right
+            right("f", speed)
+        if direction == "bl":
+            left("b", speed)
+        if direction == "br":
+            right("b", speed)
+
+        if direction == "s":
+            enable_pwm.stop()
+            enable_pwm2.stop()
+
+    except KeyboardInterrupt:
+        GPIO.output(motor_A_in2, GPIO.LOW)
+        GPIO.output(motor_B_in2, GPIO.LOW)
+        GPIO.output(motor_A_in1, GPIO.LOW)
+        GPIO.output(motor_B_in1, GPIO.LOW)
+        enable_pwm.stop()
+        enable_pwm2.stop()
+
+        GPIO.cleanup()
+        print("Done")
+        break
+
---
+++ navigation/control/step_count_both_motors.py
@@ -0,0 +1,61 @@
+import gpiozero
+import time
+
+"""
+WILL NOT WORK TO ENGAGE BOTH MOTORS (for some reason I need to find out)
+
+"""
+# RIGHT MOTOR (2)
+PIN_MOTOR2_IN1 = 23 # LOW - good
+PIN_MOTOR2_IN2 = 24 # LOW -good 
+PIN_MOTOR2_PWM_ENABLE = 9 # LOW - good 
+PIN_MOTOR2_A_OUT = 14# LOW - good 
+PIN_MOTOR2_B_OUT = 15 # LOW - good
+
+pwm2 = gpiozero.PWMOutputDevice(pin=PIN_MOTOR2_PWM_ENABLE,active_high=True,initial_value=0,frequency=100)
+dir3 = gpiozero.OutputDevice(pin=PIN_MOTOR2_IN1)
+dir4 = gpiozero.OutputDevice(pin=PIN_MOTOR2_IN2)
+
+encoder2 = gpiozero.RotaryEncoder(a=PIN_MOTOR2_A_OUT, b=PIN_MOTOR2_B_OUT,max_steps=100000) 
+
+
+
+# LEFT MOTOR (1)
+PIN_MOTOR1_IN1 = 17 # LOW - good
+PIN_MOTOR1_IN2 = 27 # LOW -good 
+PIN_MOTOR1_PWM_ENABLE = 18 # LOW - good 
+PIN_MOTOR1_A_OUT = 21# LOW - good 
+PIN_MOTOR1_B_OUT = 20 # LOW - good
+
+pwm1 = gpiozero.PWMOutputDevice(pin=PIN_MOTOR1_PWM_ENABLE,active_high=True,initial_value=0,frequency=100)
+dir1 = gpiozero.OutputDevice(pin=PIN_MOTOR1_IN1)
+dir2 = gpiozero.OutputDevice(pin=PIN_MOTOR1_IN2)
+
+encoder1 = gpiozero.RotaryEncoder(a=PIN_MOTOR1_A_OUT, b=PIN_MOTOR1_B_OUT,max_steps=100000) 
+
+
+encoder2.steps = 0
+encoder1.steps = 0
+for j in range(10):
+    pwm2.value = j/10
+    dir3.value = not dir3.value
+    dir4.value = not dir3.value
+    print('2----Duty cycle:',pwm2.value,'Direction:',dir3.value)
+
+    pwm1.value = j/10
+    dir1.value = not dir1.value
+    dir2.value = not dir2.value
+    print('1----Duty cycle:',pwm1.value,'Direction:',dir1.value)
+
+
+    time.sleep(5.0)
+    print('2----Counter:',encoder2.steps,'Speed:',(encoder2.steps)/5.0,'steps per second\n')
+    encoder2.steps = 0
+
+
+    print('1----Counter:',encoder1.steps,'Speed:',(encoder1.steps)/5.0,'steps per second\n')
+    encoder2.steps = 0
+
+pwm2.value = 0 
+pwm1.value = 0
+
