0b1c02797239dc1248f78100dd1bf6248120c017
elim0064 <elim0064@student.monash.edu>
2024-08-26 19:23:32 +1000

    Create tracking_instantiation.py

---
+++ navigation/control/tracking_instantiation.py
@@ -0,0 +1,25 @@
+
+
+
+# while True:
+#     command = input("Enter your command: ")
+
+
+import cv2
+import numpy as np
+
+from green_ball_tracker import  General_control,Region_number
+
+detector = General_control()
+detector.track_balls()
+
+
+# while True:
+#     detector.track_balls()
+#     print(detector.inCentre)
+
+
+
+
+
+    
